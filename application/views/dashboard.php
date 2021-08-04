d06477c398aa5e00f4f60c3f518d2636686c4d36
Fitra Arrafiq <fitraarrafiq@gmail.com>
2021-08-04 15:55:52 +0700

    new update

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -52,7 +52,7 @@
 
 <!-- Diagram Komparasi Karakter -->
 <!-- Chart code -->
-<script>
+<!-- <script>
     var chart = AmCharts.makeChart("karakter_tanggungjawab", {
         "type": "radar",
         "theme": "none",
@@ -81,7 +81,7 @@
         "categoryField": "country",
 
     });
-</script>
+</script> -->
 
 <section class="content">
     <div class="container-fluid">
@@ -167,7 +167,7 @@
 
         <!-- CPU Usage -->
         <div class="row clearfix">
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <div class="card">
                     <div class="header">
                         <div class="row clearfix">
@@ -195,34 +195,6 @@
                 </div>
             </div>
 
-            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                <div class="card">
-                    <div class="header">
-                        <div class="row clearfix">
-                            <div class="col-xs-12 col-sm-12">
-                                <h2>Komparasi Data Karakter</h2>
-                                <small> Statistik karakater Tanggung Jawab</small>
-                            </div>
-
-                        </div>
-                        <ul class="header-dropdown m-r--5">
-                            <li class="dropdown">
-                                <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                    <i class="material-icons">more_vert</i>
-                                </a>
-                                <ul class="dropdown-menu pull-right">
-                                    <li><a href="javascript:void(0);">Action</a></li>
-                                    <li><a href="javascript:void(0);">Another action</a></li>
-                                    <li><a href="javascript:void(0);">Something else here</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="body">
-                        <div id="karakter_tanggungjawab" class="dashboard-flot-chart"></div>
-                    </div>
-                </div>
-            </div>
 
         </div>
         <!-- #END# CPU Usage -->
